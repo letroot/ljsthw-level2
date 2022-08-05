89e29433a11d503ef95f41e962ca8eb8106e2583
Daniel Adeyemo <adeyemodaniel10@gmail.com>
2022-08-05 13:21:00 +0100

    state machine for handling comma_in_quotes use case

--- 01-parse-csv/parsecsv.js
+++ 01-parse-csv/parsecsv.js
@@ -27,39 +27,74 @@ const writeTestFile = async (samples, path) => {
   });
 };
 
-spectrum(async (err, samples) => {
-  // writeTestFile(samples, TEST_FILE);
+// spectrum(async (err, samples) => {
+// writeTestFile(samples, TEST_FILE);
 
-  for (let sample of samples) {
-    const raw_csv = sample.csv.toString();
-    // console.log("RAW CSV:\n", raw_csv);
+// for (let sample of samples) {
+//   const raw_csv = sample.csv.toString();
+//   // console.log("RAW CSV:\n", raw_csv);
 
-    const good = await csv(raw_csv);
-    const ours = simpleParse(raw_csv);
-    console.log(ours);
+//   const good = await csv(raw_csv);
+//   const ours = simpleParse(raw_csv);
+//   console.log(ours);
 
-    // if (!equal(ours, good)) {
-    //   console.error("FAIL EXPECTED", good, "\nGOT", ours);
-    //   process.exit(1);
-    // }
-    // // const parsed = await simple_parse(raw_csv);
-    // console.log("PARSED:\n", parsed);
-  }
-});
+// if (!equal(ours, good)) {
+//   console.error("FAIL EXPECTED", good, "\nGOT", ours);
+//   process.exit(1);
+// }
+// // const parsed = await simple_parse(raw_csv);
+// console.log("PARSED:\n", parsed);
+//   }
+// });
 
-// const roughStuff = () => {
-//    console.log(testObject)
-//   let first = testObject[0];
 
+// const simpleParse = (raw_csv) => {
+//   const DELIMITERS = { newline: "\n", carriage: "\r\n" };
+//   const SYMBOLS = { escapted_quotes: '""', quotes: '"' };
+
+//   let header = raw_csv.split(DELIMITERS.newline)[0];
+//   let headerArray = raw_csv.split(DELIMITERS.newline)[0].split(",");
+
+//   let parsed = {};
+//   headerArray.map((item) => (parsed[item.trim()] = ""));
+
+//   let row = [];
+//   let restString = raw_csv.substr(header.length + 1, raw_csv.length);
+
+//   console.log("rest of string", restString);
+
+//   return parsed;
 // };
 
-// roughStuff();
+let string = 'John,Doe,120 any st.,"Anytown, WW",08123';
 
-const simpleParse = (raw_csv) => {
-  const DELIMITERS = { newline: "\n", carriage: "\r\n" };
+function testthis(string) {
+  const SYMBOLS = { quotes: '"', comma: "," };
+  let text = [];
+  let result = [];
+  let isTextQuote = false;
 
-  let header = raw_csv.split(DELIMITERS.newline)[0].split(',');
-  let parsed = {};
-  header.map(item => parsed[item.trim()] = "");
-  return parsed;
-};
+  for (let char of string) {
+    if (char === SYMBOLS.quotes && !isTextQuote) {
+      text.push(char);
+      isTextQuote = true;
+    }
+    else if (isTextQuote && char === SYMBOLS.comma) {
+      text.push(char);
+    }
+    else if (isTextQuote && char === SYMBOLS.quotes) {
+      text.push(char);
+      isTextQuote = false;
+    }
+    else if (!isTextQuote && char === SYMBOLS.comma) {
+      result.push(text);
+      text = [];
+    }
+    else {
+      text.push(char)
+    }
+  }
+  return result.map(arr => arr.join(''))
+}
+
+console.log(testthis(string));
